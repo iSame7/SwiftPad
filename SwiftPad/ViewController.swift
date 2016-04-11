73cfd2dbf87510e9eb5412e9b7ebc6934cbbbc1d
iSame7 <mabrouksameh@gmail.com>
2016-04-11 15:33:29 +0200

    Initial Commit

---
+++ SwiftPad/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftPad
+//
+//  Created by Sameh Mabrouk on 4/11/16.
+//  Copyright © 2016 smapps. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
